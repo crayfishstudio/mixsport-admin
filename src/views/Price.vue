464101047d5507775e45ed594ba8db4013434d11
anna <ana_luba@ukr.net>
2021-10-09 21:38:03 +0300

    vuetify settings

---
+++ src/views/Price.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="price">
+    <p>sdfghjkjhgfd</p>
+
+    <v-btn
+      color="primary"
+      elevation="2"
+      x-large
+    >
+      Создать
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
